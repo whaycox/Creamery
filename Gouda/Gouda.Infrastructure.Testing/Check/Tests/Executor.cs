610cacd74b483adbc58f97800fbb33cb1b294a77
whaycox <whaycox@gmail.com>
2019-02-05 19:11:44 -0800

    Added basic test stubs.

---
+++ Gouda/Gouda.Infrastructure.Testing/Check/Tests/Executor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gouda.Infrastructure.Check.Tests
+{
+    [TestClass]
+    public class Executor
+    {
+        [TestMethod]
+        public void ReturnsFailureOnError()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ReturnsSuccessNormally()
+        {
+            throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void ProcessesHeartbeatCheck()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
