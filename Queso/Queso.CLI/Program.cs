50c6bcc65d7e42a2e1fae285f2138079bbc1de21
whaycox <whaycox@gmail.com>
2019-03-14 21:59:37 -0700

    Added a rudimentary CLI to resurrect my poor paladin.

---
+++ Queso/Queso.CLI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Queso.Application.Character;
+using Queso.Infrastructure.Character;
+
+namespace Queso.CLI
+{
+    class Program
+    {
+        private static ICharacter Character = new CharacterProvider();
+
+        static void Main(string[] args)
+        {
+            Character.Resurrect(args[0]);
+        }
+    }
+}
