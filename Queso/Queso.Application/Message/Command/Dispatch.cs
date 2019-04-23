c4848814b44ef57bba61c65f9bf55162df1aae96
whaycox <whaycox@gmail.com>
2019-04-22 20:45:01 -0700

    And added back a file.

---
+++ Queso/Queso.Application/Message/Command/Dispatch.cs
@@ -0,0 +1,22 @@
+﻿using Curds.Application.Message.Dispatch;
+using Curds.Application.Message;
+using System;
+using System.Collections.Generic;
+
+namespace Queso.Application.Message.Command
+{
+    public class Dispatch : BaseDispatch<QuesoApplication>
+    {
+        public Character.ResurrectDefinition Resurrect => Lookup<Character.ResurrectDefinition>();
+
+        public Dispatch(QuesoApplication application)
+            : base(application)
+        { }
+
+        protected override Dictionary<Type, BaseMessageDefinition<QuesoApplication>> BuildMapping(Dictionary<Type, BaseMessageDefinition<QuesoApplication>> requestMap)
+        {
+            requestMap.Add(typeof(Character.ResurrectDefinition), new Character.ResurrectDefinition(Application));
+            return requestMap;
+        }
+    }
+}
