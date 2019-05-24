0f06ad192e89e2b19951c7c8982a7f195a14a3bc
whaycox <whaycox@gmail.com>
2019-05-23 20:17:52 -0700

    Finished up CSV parsing tests.

--- Curds/Curds.Core.Tests/Parsing/CSV/Template/ICSVParser.cs
+++ Curds/Curds.Core.Tests/Parsing/CSV/Template/ICSVParser.cs
@@ -1,27 +1,44 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace Curds.Parsing.CSV.Template
 {
+    using Abstraction;
     using Domain;
     using Reader.Domain;
 
     public abstract class ICSVParser<T> : Test<T> where T : Abstraction.ICSVParser
     {
+        private const char WeirdSeparator = 'þ';
+        private const char WeirdQualifier = 'ÿ';
+
         private static Encoding DefaultEncoding => ReaderOptions.DefaultEncoding;
         private static Stream TextStream(string text, Encoding textEncoding) => new MemoryStream(textEncoding.GetBytes(text));
+
+        private ICSVOptions BuildOptions(char? separator, char? qualifier)
+        {
+            CSVOptions toReturn = new CSVOptions();
+            if (separator.HasValue)
+                toReturn.Separator = separator.Value;
+            if (qualifier.HasValue)
+                toReturn.Qualifier = qualifier.Value;
+            return toReturn;
+        }
+
+        protected abstract T BuildWithOptions(ICSVOptions options);
 
         [TestMethod]
         public void Simple()
         {
-            VerifySimpleParsed(TestObject.Parse(TextStream(SimpleCSV, DefaultEncoding)));
+            using (Stream testStream = TextStream(SimpleCSV, DefaultEncoding))
+                VerifySimpleParsed(TestObject.Parse(testStream));
         }
         private const string SimpleCSV = "Row1First,Row1Second,Row1Third,Row1Fourth,\r\nRow2First,Row2Second,Row2Third\r\nRow3First,Row3Second,,Row3Third\r\n,Row4First";
-        protected void VerifySimpleParsed(IEnumerable<Row> rows)
+        private void VerifySimpleParsed(IEnumerable<Row> rows)
         {
             List<Row> parsed = rows.ToList();
             Assert.AreEqual(4, parsed.Count);
@@ -65,6 +82,127 @@ namespace Curds.Parsing.CSV.Template
             Assert.AreEqual("Row4First", cells[1].Value);
         }
 
+        [TestMethod]
+        public void Separator()
+        {
+            using (Stream testStream = TextStream(SeparatorCSV, DefaultEncoding))
+                VerifySeparatorParsed(TestObject.Parse(testStream));
+        }
+        private const string SeparatorCSV = "\"Row1,TheFirst\",\"Row1,TheSecond\",\"Row1,\"\"VeryQuotable\"\"TheThird\"\r\nRow2First,Row2Second,Row2Third\r\n\"Row3,\"\"VeryQuotable\"\"TheFirst\",\"Row3,TheSecond\"";
+        private void VerifySeparatorParsed(IEnumerable<Row> rows)
+        {
+            List<Row> parsed = rows.ToList();
+            Assert.AreEqual(3, parsed.Count);
+            VerifySeparatorFirst(parsed[0]);
+            VerifySeparatorSecond(parsed[1]);
+            VerifySeparatorThird(parsed[2]);
+        }
+        private void VerifySeparatorFirst(Row first)
+        {
+            List<Cell> cells = first.RetrieveCells();
+            Assert.AreEqual(3, cells.Count);
+            Assert.AreEqual("Row1,TheFirst", cells[0].Value);
+            Assert.AreEqual("Row1,TheSecond", cells[1].Value);
+            Assert.AreEqual("Row1,\"VeryQuotable\"TheThird", cells[2].Value);
+        }
+        private void VerifySeparatorSecond(Row second)
+        {
+            List<Cell> cells = second.RetrieveCells();
+            Assert.AreEqual(3, cells.Count);
+            Assert.AreEqual("Row2First", cells[0].Value);
+            Assert.AreEqual("Row2Second", cells[1].Value);
+            Assert.AreEqual("Row2Third", cells[2].Value);
+        }
+        private void VerifySeparatorThird(Row third)
+        {
+            List<Cell> cells = third.RetrieveCells();
+            Assert.AreEqual(2, cells.Count);
+            Assert.AreEqual("Row3,\"VeryQuotable\"TheFirst", cells[0].Value);
+            Assert.AreEqual("Row3,TheSecond", cells[1].Value);
+        }
 
+        [TestMethod]
+        public void CustomSeparator()
+        {
+            CustomSeparatorOnly();
+            CustomQualifierOnly();
+            BothCustom();
+        }
+        private void VerifyCustomSeparator(Row parsed)
+        {
+            List<Cell> cells = parsed.RetrieveCells();
+            Assert.AreEqual(4, cells.Count);
+            Assert.AreEqual("FirstValue", cells[0].Value);
+            Assert.AreEqual("SecoþndÿValue", cells[1].Value);
+            Assert.AreEqual("Third\r\nValue", cells[2].Value);
+            Assert.AreEqual(string.Empty, cells[3].Value);
+        }
+
+        private void CustomSeparatorOnly()
+        {
+            T parser = BuildWithOptions(BuildOptions(WeirdSeparator, null));
+            using (Stream testStream = TextStream(SeparatorOnlyCSV, DefaultEncoding))
+            {
+                List<Row> parsed = parser.Parse(testStream).ToList();
+                Assert.AreEqual(1, parsed.Count);
+                VerifyCustomSeparator(parsed[0]);
+            }
+        }
+        private const string SeparatorOnlyCSV = "FirstValueþ\"SecoþndÿValue\"þ\"Third\r\nValue\"þ";
+
+        private void CustomQualifierOnly()
+        {
+            T parser = BuildWithOptions(BuildOptions(null, WeirdQualifier));
+            using (Stream testStream = TextStream(QualifierOnlyCSV, DefaultEncoding))
+            {
+                List<Row> parsed = parser.Parse(testStream).ToList();
+                Assert.AreEqual(1, parsed.Count);
+                VerifyCustomSeparator(parsed[0]);
+            }
+        }
+        private const string QualifierOnlyCSV = "FirstValue,ÿSecoþndÿÿValueÿ,ÿThird\r\nValueÿ,";
+
+        private void BothCustom()
+        {
+            T parser = BuildWithOptions(BuildOptions(WeirdSeparator, WeirdQualifier));
+            using (Stream testStream = TextStream(BothCustomCSV, DefaultEncoding))
+            {
+                List<Row> parsed = parser.Parse(testStream).ToList();
+                Assert.AreEqual(1, parsed.Count);
+                VerifyCustomSeparator(parsed[0]);
+            }
+        }
+        private const string BothCustomCSV = "FirstValueþÿSecoþndÿÿValueÿþÿThird\r\nValueÿþ";
+
+        [TestMethod]
+        public void Large()
+        {
+            using (Stream testStream = Files.Manifest.LargeStream)
+            {
+                List<Row> parsed = TestObject.Parse(testStream).ToList();
+                Assert.AreEqual(LargeExpectedRows, parsed.Count);
+                Assert.IsFalse(parsed.Where(p => p.RetrieveCells().Count != LargeExpectedWidth).Any());
+                int[] totals = new int[LargeExpectedWidth];
+                for (int i = 0; i < LargeRowsToSum; i++)
+                    AddRowToTotals(parsed[i], totals);
+                CompareWithFinalRow(parsed[LargeExpectedRows - 1], totals);
+            }
+        }
+        private const int LargeExpectedRows = 1003;
+        private const int LargeExpectedWidth = 15;
+        private const int LargeRowsToSum = 1000;
+        private int ParseCell(Cell cell) => int.Parse(cell.Value);
+        private void AddRowToTotals(Row row, int[] totals)
+        {
+            List<Cell> cells = row.RetrieveCells();
+            for (int i = 0; i < LargeExpectedWidth; i++)
+                totals[i] += ParseCell(cells[i]);
+        }
+        private void CompareWithFinalRow(Row final, int[] totals)
+        {
+            List<Cell> cells = final.RetrieveCells();
+            for (int i = 0; i < LargeExpectedWidth; i++)
+                Assert.AreEqual(totals[i], ParseCell(cells[i]));
+        }
     }
 }
--- Curds/Curds.Core.Tests/Parsing/CSV/Tests/Parser.cs
+++ Curds/Curds.Core.Tests/Parsing/CSV/Tests/Parser.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Curds.Parsing.CSV.Tests
 {
+    using Abstraction;
+
     [TestClass]
     public class Parser : Template.ICSVParser<Implementation.Parser>
     {
-        private static Abstraction.ICSVOptions DefaultOptions => new Domain.CSVOptions();
+        private static ICSVOptions DefaultOptions => new Domain.CSVOptions();
 
         protected override Implementation.Parser TestObject { get; } = new Implementation.Parser(DefaultOptions);
+
+        protected override Implementation.Parser BuildWithOptions(ICSVOptions options) => new Implementation.Parser(options);
 
     }
 }
