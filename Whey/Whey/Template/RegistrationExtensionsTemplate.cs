e47b1184ce6285be6465bd15ff782fc2f534fd79
whaycox <whaycox@gmail.com>
2020-01-01 23:13:49 -0800

    Generated Whey package.

--- Whey/Whey/Template/RegistrationExtensionsTemplate.cs
+++ Whey/Whey/Template/RegistrationExtensionsTemplate.cs
@@ -8,6 +8,7 @@ namespace Whey.Template
     public abstract class RegistrationExtensionsTemplate
     {
         protected IServiceCollection TestServiceCollection = new ServiceCollection();
+        protected IServiceProvider TestServiceProvider => TestServiceCollection.BuildServiceProvider();
 
         protected void VerifyServiceWasRegistered(Type abstractionType, Type implementationType, ServiceLifetime lifetime)
         {
